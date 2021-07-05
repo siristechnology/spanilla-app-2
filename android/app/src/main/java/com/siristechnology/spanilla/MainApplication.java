b6903b6a1fc578d83b3c9d67d0af06e5e2ec9b24
Suraj Shrestha <syuraj@gmail.com>
2021-07-04 20:12:40 -0400

    app renaming

--- android/app/src/main/java/com/siristechnology/spanilla/MainApplication.java
+++ android/app/src/main/java/com/siristechnology/spanilla/MainApplication.java
@@ -1,4 +1,4 @@
-package com.spanilla3;
+package com.siristechnology.spanilla;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.spanilla3.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.spanilla.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
