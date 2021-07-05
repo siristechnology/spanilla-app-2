f2372948cc81cc37fe52e63970570078e4c2f8f9
Suraj Shrestha <syuraj@gmail.com>
2021-07-04 22:26:45 -0400

    include beta.keystore

--- prebuild_script.sh
+++ prebuild_script.sh
@@ -3,6 +3,6 @@
 TIME_STAMP=$(date +%s)
 BUILD_GRADLE=./android/app/build.gradle
 
-sed -i '' 's/versionCode .*$/versionCode '$TIME_STAMP'/' $BUILD_GRADLE
+sed -i '' -e 's/versionCode .*$/versionCode '$TIME_STAMP'/' $BUILD_GRADLE
 
-sed -i '' 's/versionName "[^"]*"/versionName "'$TIME_STAMP'"/' $BUILD_GRADLE
+sed -i '' -e 's/versionName "[^"]*"/versionName "'$TIME_STAMP'"/' $BUILD_GRADLE
